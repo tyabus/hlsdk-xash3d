fb316ee716be437479fd365b4bbf522baee22d38
tyabus <tyabustest@gmail.com>
2019-03-27 17:57:41 +0300

    Add cleansuit scientist

--- dlls/Android.mk
+++ dlls/Android.mk
@@ -129,6 +129,7 @@ LOCAL_SRC_FILES := agrunt.cpp airtank.cpp \
            xen.cpp \
            zombie.cpp \
            prop.cpp \
+	   cleansuit_scientist.cpp \
 	   poisoncrab.cpp \
            gravgun.cpp \
            ar2.cpp \
