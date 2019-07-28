798ab65580976d53819cf2b1ce2a5765f989e8aa
tyabus <tyabustest@gmail.com>
2019-07-28 13:04:28 +0300

    Fix gauss for coop

--- dlls/gauss.cpp
+++ dlls/gauss.cpp
@@ -25,6 +25,7 @@
 #include "shake.h"
 #include "gamerules.h"
 #include "game.h"
+#include "coop_util.h"
 
 #define	GAUSS_PRIMARY_CHARGE_VOLUME	256// how loud gauss is while charging
 #define GAUSS_PRIMARY_FIRE_VOLUME	450// how loud gauss is when discharged
@@ -353,7 +354,7 @@ void CGauss::StartFire( void )
 			m_pPlayer->pev->velocity = m_pPlayer->pev->velocity - gpGlobals->v_forward * flDamage * 5;
 		}
 
-		if( !g_pGameRules->IsMultiplayer() )
+		if( mp_coop.value || !g_pGameRules->IsMultiplayer() )
 		{
 			// in deathmatch, gauss can pop you up into the air. Not in single play.
 			m_pPlayer->pev->velocity.z = flZVel;
