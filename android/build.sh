6a546cf5bb03c6a3a39d7663f9d03d38e96654b0
tyabus <tyabustest@gmail.com>
2019-10-11 10:18:44 +0300

    Update version

--- android/build.sh
+++ android/build.sh
@@ -3,5 +3,5 @@ mkdir -p assets
 python makepak.py pak/ assets/extras.pak
 ndk-build NDK_TOOLCHAIN_VERSION=4.9 NDK_DEBUG=1 V=1 -j5 APP_CFLAGS="-w"
 ant debug
-#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../myks.keystore bin/mod-unsigned.apk xashdroid -tsa https://timestamp.geotrust.com/tsa
+#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./ggm.keystore bin/ggm-debug.apk ggm
 #zipalign 4 bin/cs16-client-unsigned.apk bin/mod.apk
