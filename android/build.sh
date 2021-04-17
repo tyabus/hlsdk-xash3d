deb73f17f802ef9f3ed1a49219e783b0403108bb
tyabus <tyabustest@gmail.com>
2021-04-17 13:19:14 +0600

    Use python2 for makepak.py, because most systems has 3 as default

--- android/build.sh
+++ android/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p assets
-python makepak.py pak/ assets/extras.pak
+python2 makepak.py pak/ assets/extras.pak
 ndk-build NDK_TOOLCHAIN_VERSION=4.9 NDK_DEBUG=1 V=1 -j5 APP_CFLAGS="-w"
 ant debug
 #jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./ggm.keystore bin/ggm-debug.apk ggm
