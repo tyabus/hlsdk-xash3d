485d535a66c0274aa6d2e7c855ae5a81a6d6fff4
tyabus <tyabustest@gmail.com>
2021-04-18 18:45:25 +0600

    zombie.cpp: same on both sides

--- dlls/zombie.cpp
+++ dlls/zombie.cpp
@@ -332,7 +332,7 @@ int CZombie::IgnoreConditions( void )
 {
 	int iIgnore = CBaseMonster::IgnoreConditions();
 
-	if( ( m_Activity == ACT_MELEE_ATTACK1 ) || ( m_Activity == ACT_MELEE_ATTACK1 ) )
+	if( m_Activity == ACT_MELEE_ATTACK1 )
 	{
 		if( m_flNextFlinch >= gpGlobals->time )
 			iIgnore |= ( bits_COND_LIGHT_DAMAGE | bits_COND_HEAVY_DAMAGE );
