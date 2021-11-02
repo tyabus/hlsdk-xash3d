5de337d3e4a17a37b382981f4011c4bd6eff6936
tyabus <tyabustest@gmail.com>
2021-11-03 02:24:11 +0600

    Stop counting player as the enemy if he went to spectators

--- dlls/monsters.cpp
+++ dlls/monsters.cpp
@@ -1055,7 +1055,7 @@ int CBaseMonster::CheckEnemy( CBaseEntity *pEnemy )
 	else
 		ClearConditions( bits_COND_ENEMY_OCCLUDED );
 
-	if( !pEnemy->IsAlive() )
+	if( !pEnemy->IsAlive() || ( FClassnameIs( pEnemy->pev, "player" ) && pEnemy->pev->flags & FL_SPECTATOR ) )
 	{
 		SetConditions( bits_COND_ENEMY_DEAD );
 		ClearConditions( bits_COND_SEE_ENEMY | bits_COND_ENEMY_OCCLUDED );
